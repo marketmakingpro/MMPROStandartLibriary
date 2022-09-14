8f35ade0ef7eefb05642beab93f8e8982a5a6498
ddzaytsev <DDZaytsev@sberbank.ru>
2022-09-14 15:04:23 +0300

    design update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,24 @@
 import React, {useEffect, useState} from "react";
 import './index.css'
-import logo from '../../images/MMProLogo.svg'
+import Logo from '../../icons/MMPROLogo';
 import logoSmall from '../../images/MMProLogoSmall.svg'
 import {LocaleSelector} from "../LocaleSelector";
 import {Link} from "react-router-dom";
 import WalletConnector, {HeaderButton} from "../WalletConnector";
+import styled from 'styled-components'
 
 const HeaderDefaultProps = {
   logoHref: 'https://marketmaking.pro/'
 }
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 340px;
+  z-index: 5;
+  padding: 25px 65px;
+  border-bottom: 1px solid #181833;
+`
 
 const Header = (props: { headerButtons?: React.ReactElement[], connectorButtons: HeaderButton[], logoHref?: string, hideWalletConnector?: boolean, locales: string[], pages?: { title: string, url: string }[] }) => {
   const {locales, pages, logoHref, hideWalletConnector, headerButtons, connectorButtons} = props
@@ -19,27 +29,26 @@ const Header = (props: { headerButtons?: React.ReactElement[], connectorButtons:
   }, [])
 
   return (
-    <>
-      <header className="px-4 mx-auto py-4" style={{minWidth: 340, zIndex: 5}}>
-        <div className="flex flex-row justify-between items-center w-full">
-          <div className={'logo-and-tabs'}>
-            <a href={logoHref}>
-              <img
-                src={logo}
-                width="180"
-                className="cursor-pointer logo-large"
-                alt="mmpro logo"
-              />
-              <img
-                src={logoSmall}
-                width="180"
-                className="cursor-pointer logo-small"
-                alt="mmpro logo"
-              />
-            </a>
-            <div className={'separator'}/>
-            <div className={'tabs'}>
-              {(pages !== undefined && pages.length > 0) &&
+    <HeaderContainer>
+      <div className="flex flex-row justify-between items-center w-full">
+        <div className={'logo-and-tabs'}>
+          <a href={logoHref}>
+            <Logo/>
+            {/*<img*/}
+            {/*  src={logo}*/}
+            {/*  width="180"*/}
+            {/*  className="cursor-pointer logo-large"*/}
+            {/*  alt="mmpro logo"*/}
+            {/*/>*/}
+            {/*<img*/}
+            {/*  src={logoSmall}*/}
+            {/*  width="180"*/}
+            {/*  className="cursor-pointer logo-small"*/}
+            {/*  alt="mmpro logo"*/}
+            {/*/>*/}
+          </a>
+          <div className={'tabs'}>
+            {(pages !== undefined && pages.length > 0) &&
               <>
                 {pages.map(page => (
                   <Link
@@ -55,24 +64,23 @@ const Header = (props: { headerButtons?: React.ReactElement[], connectorButtons:
                   </Link>
                 ))}
               </>
-              }
-            </div>
-          </div>
-
-          <div className={'control-strip'}>
-            {headerButtons &&
-              headerButtons.map(button => button)
-            }
-            {locales.length > 1 &&
-            <LocaleSelector locales={locales}/>
-            }
-            {!hideWalletConnector &&
-            <WalletConnector buttons={connectorButtons}/>
             }
           </div>
         </div>
-      </header>
-    </>
+
+        <div className={'control-strip'}>
+          {headerButtons &&
+            headerButtons.map(button => button)
+          }
+          {locales.length > 1 &&
+            <LocaleSelector locales={locales}/>
+          }
+          {!hideWalletConnector &&
+            <WalletConnector buttons={connectorButtons}/>
+          }
+        </div>
+      </div>
+    </HeaderContainer>
   );
 };
 
