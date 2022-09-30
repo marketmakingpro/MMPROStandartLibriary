e8fb0558315d62677a7bd39c7a99e27c2655d9ba
ddzaytsev <DDZaytsev@sberbank.ru>
2022-09-30 21:03:44 +0300

    add 404 page

---
+++ components/404/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './index.scss';
+import TrustButton from "../TrustButton";
+import Text from "../Text";
+import {useHistory} from "react-router-dom";
+
+const NoPageError = () => {
+  const history = useHistory()
+  return (
+    <div className='container-error'>
+      <div className='wrapper'>
+        <div className="one">
+          <div className="content">
+            <span className="piece"/>
+            <span className="piece"/>
+            <span className="piece"/>
+          </div>
+        </div>
+        <div className="two" >
+          <div className="content">
+            <span className="piece" />
+            <span className="piece" />
+            <span className="piece" />
+          </div>
+        </div>
+        <div className="three">
+          <div className="content">
+            <span className="piece" />
+            <span className="piece" />
+          </div>
+        </div>
+      </div>
+      <div className='text-wrapper'>
+        <div className="p404">404</div>
+        <div className="text">
+          <Text fontWeight={400} fontSize={24}>Page not found :(</Text>
+          <div className='mb-4'/>
+          <TrustButton isValid style={"green"} onClick={() => history.push('/account')}>Redirect</TrustButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoPageError;
