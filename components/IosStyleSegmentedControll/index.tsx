595be63c18f24a9dc180a785d00cb83e0ad72ec1
ddzaytsev <DDZaytsev@sberbank.ru>
2022-11-16 19:18:12 +0300

    add IOSToggle

---
+++ components/IosStyleSegmentedControll/index.tsx
@@ -0,0 +1,52 @@
+import React, {useContext, useState} from "react";
+import './index.css'
+
+type IosStyleSegmentedControlPropType = {
+    width: number;
+    buttons: string[];
+    firstSelectedIndex: number;
+    onChange: (buttonIndex: number) => void;
+}
+
+const IosStyleSegmentedControll = (props: IosStyleSegmentedControlPropType) => {
+    const { width, buttons, firstSelectedIndex, onChange } = props;
+    const buttonWidthInPercent = 100 / buttons.length;
+    const [selectedButton, setSelectedButton] = useState<number>(firstSelectedIndex);
+
+    const onButtonClick = (index: number) => {
+        // @ts-ignore
+        setSelectedButton(index);
+        // @ts-ignore
+        onChange(index);
+    };
+
+    return (
+        <div className={'IosStyleRadioButton'} style={{ width }}>
+            <div
+                className={'selector-bg'}
+                style={{
+                    width: `calc(${buttonWidthInPercent}% - 4px)`,
+                    left: `calc(${selectedButton * buttonWidthInPercent}% + 2px)`,
+                }}
+            />
+            {buttons.map((buttonTitle, index) => {
+                return (
+                    <button
+                        key={buttonTitle}
+                        type="button"
+                        value={index}
+                        onClick={() => onButtonClick(index)}
+                        className={`selectableButton ${
+                            selectedButton === index && 'selectableButtonSelected'
+                        }`}
+                        style={{ width: `${buttonWidthInPercent}%` }}
+                    >
+                        {buttonTitle}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
+
+export default IosStyleSegmentedControll
