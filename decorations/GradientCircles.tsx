ac63180c364328c4a0210c14fbaa7e4809352852
danilitmo123 <danilitmo@gmail.com>
2023-08-31 14:00:41 +0300

    fix gradient

--- decorations/GradientCircles.tsx
+++ decorations/GradientCircles.tsx
@@ -1,50 +1,31 @@
 import React from "react";
-
+import PurpleCircle from '../../icons/PurleCircle.png'
+import GreenCircle from '../../icons/GreenCircle.png'
 import styled from "styled-components";
 
-const BlueGreenGradientCircle = styled.div`
+const BlueGreenGradientCircle = styled.img`
   position: absolute;
-  width: 55%;
+  width: 70%;
   padding-bottom: 55%;
-  background: linear-gradient(228.62deg, #5790FF 12.13%, #A5FDC3 94.47%);
-  opacity: 0.6;
   left: -20%;
-  top: 33%;
+  top: 5%;
   z-index: 0;
-  border-radius: 50%;
-  filter: blur(50px);
 `
 
-const PurpleBlueGradientCircle = styled.div`
+const PurpleBlueGradientCircle = styled.img`
   position: absolute;
-  width: 50%;
   padding-bottom: 50%;
-  right: -15%;
-  top: 60%;
-  background: linear-gradient(133.46deg, #5790FF 14.79%, rgba(165, 36, 226, 0) 103.42%);
-  opacity: 0.6;
-  z-index: 0;
-  border-radius: 50%;
-  filter: blur(50px);
-`
-
-const DecorationContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  right: 0px;
-  bottom: 0px;
+  width: 70%;
+  right: -10%;
+  top: 40%;
   z-index: 0;
 `
-
-
 const GradientCircles = () => {
   return (
-    <DecorationContainer>
-      <BlueGreenGradientCircle/>
-      <PurpleBlueGradientCircle/>
-    </DecorationContainer>
+    <>
+      <BlueGreenGradientCircle src={PurpleCircle}/>
+      <PurpleBlueGradientCircle src={GreenCircle} />
+    </>
   );
 };
 
